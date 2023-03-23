78b3b1e76dc08028f662de36eeb8bc7a99c64fd3
jhndoctor <johndoctor269@gmail.com>
2023-03-23 15:07:57 +0630

    update 26

--- 26.popup-form/app.js
+++ 26.popup-form/app.js
@@ -1,5 +1,5 @@
-const close = document.getElementById('close');
 const open = document.getElementById('open');
+const close = document.getElementById('close');
 const modal = document.getElementById('modal');
 
 open.addEventListener('click', () => modal.classList.add('show-modal'));
